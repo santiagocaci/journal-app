6fc852e69534c8748c0c9da99ee88676dee228b6
Santiago Caci <santiago.caci@gmail.com>
2022-08-15 00:06:17 -0300

    Fixed ImageGallery

--- src/journal/react-components/ImageGallery.jsx
+++ src/journal/react-components/ImageGallery.jsx
@@ -4,16 +4,17 @@ import { ImageList, ImageListItem } from '@mui/material';
 export const ImageGallery = ({ images }) => {
   return (
     <ImageList sx={{ width: '100%', height: 600 }} cols={3} rowHeight={200}>
-      {images.map((image) => (
-        <ImageListItem key={image}>
-          <img
-            src={`${image}?w=164&h=164&fit=crop&auto=format`}
-            srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            alt='image de la nota'
-            loading='lazy'
-          />
-        </ImageListItem>
-      ))}
+      {images &&
+        images.map((image) => (
+          <ImageListItem key={image}>
+            <img
+              src={`${image}?w=164&h=164&fit=crop&auto=format`}
+              srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt='image de la nota'
+              loading='lazy'
+            />
+          </ImageListItem>
+        ))}
     </ImageList>
   );
 };
